221c10518cc5466a003265e785b5d4c0e93bbfc5
Aleksander Kamiński <37936683+agkaminski@users.noreply.github.com>
2020-11-26 16:09:33 +0100

    psu: Added MCU-BOOT protocol and imxrt1176 support (#15)

--- psu/psu.c
+++ psu/psu.c
@@ -39,6 +39,15 @@
 #define SET_DATA(b, v) *((uint32_t *)((b) + 11)) = htonl(v);
 #define SET_FORMAT(b, v) (b)[6] = (v);
 
+/* MCUBoot protocol */
+#define FRAME_CMD_OUT 1
+#define FRAME_DATA 2
+#define FRAME_CMD_IN 3
+#define MCU_CMD_SIZE 32
+#define MCU_GET_PROPERTY 0x07
+#define MCU_GET_PROPERTY_RESPONSE 0xa7
+#define MCU_MAX_PAYLOAD 1016
+
 #define CMD_SIZE 17
 #define BUF_SIZE 1025
 #define INTERRUPT_SIZE 65
@@ -222,6 +231,113 @@ static int sdp_errStatus(hid_device *dev)
 		fprintf(stderr, "Failed to receive status (rc=%d)\n", rc);
 		return rc;
 	}
+
+	return SCRIPT_OK;
+}
+
+
+typedef struct {
+	unsigned char reportID;
+	unsigned char padding;
+	unsigned short size;
+	unsigned char payload[];
+} __attribute__((packed)) mcuboot_frame_t;
+
+
+typedef struct {
+	unsigned char tag;
+	unsigned char flags;
+	unsigned char reserved;
+	unsigned char paramcnt;
+	int params[];
+} __attribute__((packed)) mcuboot_cmd_t;
+
+
+static unsigned short size2LE(unsigned short size)
+{
+	if (size == htons(size))
+		return (unsigned short)((size & (unsigned short)0xff) << 8) | (size >> 8);
+
+	return size;
+}
+
+
+static unsigned int paramByteSwap(unsigned int param)
+{
+	/* If host is BE, swap bytes. Works both way */
+	if (param == htonl(param))
+		return ((param & 0xff) << 24) | ((param & 0xff00) << 8) |
+				((param & 0xff0000) >> 8) | ((param & 0xff000000) >> 24);
+
+	return param;
+}
+
+
+static int mcuboot_getProperty(hid_device *dev, int which)
+{
+	unsigned char b[sizeof(mcuboot_frame_t) + sizeof(mcuboot_cmd_t) + 2 * 4] = { 0 };
+	mcuboot_frame_t *frame = (mcuboot_frame_t *)b;
+	mcuboot_cmd_t *cmd = (mcuboot_cmd_t *)(b + sizeof(mcuboot_frame_t));
+	int rc;
+
+	frame->reportID = FRAME_CMD_OUT;
+	frame->size = size2LE(sizeof(b) - sizeof(mcuboot_frame_t));
+
+	cmd->tag = MCU_GET_PROPERTY;
+	cmd->paramcnt = 2;
+	cmd->params[0] = paramByteSwap(which);
+	cmd->params[1] = 0;
+
+	if ((rc = hid_write(dev, b, sizeof(b))) < 0) {
+		fprintf(stderr, "Failed to send get_property command (rc=%d)\n", rc);
+		return rc;
+	}
+
+	if ((rc = hid_read(dev, b, sizeof(b))) < 0) {
+		fprintf(stderr, "Failed to receive GetProperty Response (rc=%d)\n", rc);
+		return rc;
+	}
+
+	if (cmd->params[0] != 0) {
+		fprintf(stderr, "GetPropertyResponse status != 0 (%d)\n", paramByteSwap(cmd->params[0]));
+		return -1;
+	}
+
+	fprintf(stderr, "Status: %d, Property: 0x%08x\n", paramByteSwap(cmd->params[0]), paramByteSwap(cmd->params[1]));
+
+	return SCRIPT_OK;
+}
+
+
+static int mcuboot_loadImage(hid_device *dev, void *data, size_t size)
+{
+	unsigned char b[sizeof(mcuboot_frame_t) + MCU_MAX_PAYLOAD] = { 0 };
+	mcuboot_frame_t *frame = (mcuboot_frame_t *)b;
+	int rc;
+	size_t offset = 0, chunk;
+
+	while (offset < size) {
+		chunk = size - offset;
+		if (chunk > MCU_MAX_PAYLOAD)
+			chunk = MCU_MAX_PAYLOAD;
+
+		frame->reportID = FRAME_DATA;
+		frame->size = size2LE(chunk);
+
+		memcpy(frame->payload, (char *)data + offset, chunk);
+
+		if ((rc = hid_write(dev, b, sizeof(mcuboot_frame_t) + chunk)) < 0) {
+			fprintf(stderr, "Failed to send data (rc=%d)\n", rc);
+			return rc;
+		}
+
+		offset += chunk;
+
+		/* print progress */
+		fprintf(stderr, "\r - Sent (%lu/%lu) %3.0f%% ", offset, size, ((float)offset / (float)size) * 100.0f);
+	}
+
+	fprintf(stderr, " - File has been written correctly.\n");
 
 	return SCRIPT_OK;
 }
@@ -552,6 +668,61 @@ static int not_implemented_cmd(script_t *s)
 }
 
 
+static int load_image_cmd(script_t *s)
+{
+	int res;
+	script_blob_t str;
+	script_blob_t blob = SCRIPT_BLOB_EMPTY;
+	hid_device *dev = *( hid_device **)s->arg;
+
+	if (script_expect(s, script_tok_string, "String in quotes was expected") != SCRIPT_OK)
+		return SCRIPT_ERROR;
+
+	str = s->token.str;
+
+	if (get_buffer(s, 'F', str, &blob) < 0)
+		return SCRIPT_ERROR;
+
+	if (s->flags & SCRIPT_F_DRYRUN)
+		return SCRIPT_OK;
+
+	res = SCRIPT_ERROR;
+
+	if (dev)
+		res = mcuboot_loadImage(dev, blob.ptr, blob.end - blob.ptr);
+
+	close_buffer('F', &blob);
+
+	if (res == SCRIPT_OK)
+		return SCRIPT_OK;
+
+	s->errstr = "Device not available";
+
+	return SCRIPT_ERROR;
+}
+
+
+static int get_property_cmd(script_t *s)
+{
+	hid_device *dev = *(hid_device **)s->arg;
+
+	if (s->flags & SCRIPT_F_DRYRUN)
+		return SCRIPT_OK;
+
+	if (!dev) {
+		s->errstr = "Device not available";
+		return SCRIPT_ERROR;
+	}
+
+	if (mcuboot_getProperty(dev, 1) == SCRIPT_OK)
+		return SCRIPT_OK;
+
+	s->errstr = "Command failed";
+
+	return SCRIPT_ERROR;
+}
+
+
 /*
  * NOTE: because binary search is used, function names
  * must be sorted in lexical order, use upper-case,
@@ -560,13 +731,15 @@ static int not_implemented_cmd(script_t *s)
 static const script_funct_t funcs[] = {
 	{ "DCD_WRITE", not_implemented_cmd },
 	{ "ERROR_STATUS", err_status_cmd },
+	{ "GET_PROPERTY", get_property_cmd },
 	{ "JUMP_ADDRESS", jump_addr_cmd },
+	{ "LOAD_IMAGE", load_image_cmd },
 	{ "PROMPT", not_implemented_cmd },
 	{ "REBOOT", not_implemented_cmd },
 	{ "WAIT", wait_cmd },
 	{ "WRITE_FILE", write_file_cmd },
 	{ "WRITE_REGISTER", write_reg_cmd },
-	{ NULL },
+	{ NULL, NULL }
 };
 
 
